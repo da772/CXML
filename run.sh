fd3a9a50c79e60402dba5a390823c989efed586e
da772 <daniel@DESKTOP-7BLKIJ0.localdomain>
2021-01-19 08:01:20 -0800

    update ignore

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-g++ -g -c tests/test.cpp -o obj/test.o -Iinclude
-g++ -g -c tests/default_pointers.cpp -o obj/default_pointers.o -Iinclude/
-g++ -g -c tests/dynamic_pointers.cpp -o obj/dynamic_pointers.o -Iinclude/
-g++ -g obj/CXML.o obj/default_pointers.o obj/dynamic_pointers.o obj/test.o -o bin/Tests
-chmod +x bin/Tests
-./bin/Tests
-
